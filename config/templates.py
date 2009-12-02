85a6430c0f722f1f0122e4f08183f0ca6ac2d4db
Nir Yariv <niryariv@gmail.com>
2009-12-02 16:27:25 -0500

    new template with example

--- config/templates.py
+++ config/templates.py
@@ -1,7 +1,12 @@
 CODE = '''
 
 def get():
-    pass
+    msg = self.request.get("msg", None)
+    if msg is None:
+        msg = "Hi! I'm a new Weblet. "
+        msg+= "Call me with ?msg=hey to print out 'hey', "
+        msg+= "or edit me to start writing your own script."
+    self.render(msg)
     
 def post():
     pass
@@ -11,6 +16,7 @@ def put():
 
 def delete():
     pass
+
 
 '''
 
